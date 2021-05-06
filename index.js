b60ad73bc45d7fc677c20c7e20471650e7765ca2
Baldeep Kaur <>
2021-05-06 03:51:42 +0000

    Splash page set for @320, @375 and @425

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+'use strict'
+
+// our database operations
+const dbo = require('./user/databaseOps');  //T 
+// Promises-wrapped version of sqlite3
+const db = require('./user/sqlWrap');       //T 
+// functions that verify activities before putting them in database
+const act = require('./user/activity');     //T 
+
 const express = require('express');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
@@ -32,9 +41,11 @@ passport.use(new GoogleStrategy(googleLoginData, gotProfile) );
 
 
 // Let's build a server pipeline!
-
 // app is the object that implements the express server
 const app = express();
+
+// use this instead of the older body-parser
+app.use(express.json());      //T 
 
 // pipeline stage that just echos url, for debugging
 app.use('/', printURL);
@@ -115,6 +126,100 @@ app.get('/query', isAuthenticated,
     function (req, res) { 
       console.log("saw query");
       res.send('HTTP query!') });
+
+///////// QUERIES FROM FITNESS 2 BEGIN 
+
+// This is where the server recieves and responds to get /all requests
+// used for debugging - dumps whole database
+app.get('/all', isAuthenticated, 
+    async function(request, response, next) {
+      console.log("Server recieved a get /all request at", request.url);
+      let results = await dbo.get_all()
+      
+      response.send(results);
+});
+
+//************************* */
+
+// This is where the server recieves and responds to store POST requests
+app.post('/store', isAuthenticated, 
+    async function(request, response, next) {
+      console.log("Server recieved a post request at", request.url);
+
+      let activity = act.Activity(request.body)
+      await dbo.post_activity(activity)
+      
+      response.send({ message: "I got your POST request"});
+});
+
+//************************* */
+
+// This is where the server recieves and responds to  reminder GET requests
+app.get('/reminder', isAuthenticated, 
+    async function(request, response, next) {
+      console.log("Server recieved a post request at", request.url)
+      
+      let currTime = newUTCTime()
+      currTime = (new Date()).getTime()
+
+      // Get Most Recent Past Planned Activity and Delete All Past Planned Activities
+      let result = await dbo.get_most_recent_planned_activity_in_range(0, currTime)
+      await dbo.delete_past_activities_in_range(0, currTime);
+
+      if (result != null){
+        // Format Activity Object Properly
+        result.scalar = result.amount
+        result.date = result['MAX(date)']
+        // Send Client Most Recent Planned Activity from the Past
+        response.send(act.Activity(result));
+      } else {
+        response.send({message: 'All activities up to date!'});
+      }
+});
+
+//************************* */
+
+// This is where the server recieves and responds to week GET requests
+app.get('/week', isAuthenticated, 
+    async function(request, response, next) {
+      console.log("Server recieved a post request at", request.url);
+
+      let date = parseInt(request.query.date)
+      let activity = request.query.activity
+      
+      /* Get Latest Activity in DB if not provided by query params */
+      if (activity === undefined) {
+        let result = await dbo.get_most_recent_entry()
+        try {
+          activity = result.activity
+        } catch(error) {
+          activity = "none"
+        }
+      }
+      
+      /* Get Activity Data for current Date and The Week Prior */
+      let min = date - 6 * MS_IN_DAY
+      let max = date
+      let result = await dbo.get_similar_activities_in_range(activity, min, max)
+
+      /* Store Activity amounts in Buckets, Ascending by Date */
+      let data = Array.from({length: 7}, (_, i) => {
+        return { date: date - i * MS_IN_DAY, value: 0 }
+      })
+
+      /* Fill Data Buckets With Activity Amounts */
+      for(let i = 0 ; i < result.length; i++) {
+        let idx = Math.floor((date - result[i].date)/MS_IN_DAY)
+        data[idx].value += result[i].amount
+      }
+      
+      // Send Client Activity for the Se;ected Week
+      response.send(data.reverse());
+});
+
+//************************* */
+
+///////// QUERIES FROM FITNESS 2 END 
 
 // finally, file not found, if we cannot handle otherwise.
 app.use( fileNotFound );
@@ -199,5 +304,58 @@ passport.deserializeUser((userid, done) => {
     done(null, userData);
 });
 
-
 const mySecret = process.env['ClientID']
+
+///////////////////////OTHER FUNCTIONS FROM FITNESS 2 BEGIN
+
+// call the async test function for the database
+// this fills the db with test data
+// in your system, you can delete this. 
+dbo.testDB().catch(
+  function (error) {
+    console.log("error:",error);}
+);
+
+// UNORGANIZED HELPER FUNCTIONS
+
+const MS_IN_DAY = 86400000
+
+/**
+ * Convert GMT date to UTC
+ * @returns {Date} current date, but converts GMT date to UTC date
+ */
+ function newUTCTime() {
+    let gmtDate = new Date()
+    let utcDate = (new Date(gmtDate.toLocaleDateString()))
+    let utcTime = Date.UTC(
+        utcDate.getFullYear(),
+        utcDate.getMonth(),
+        utcDate.getDay()
+    )
+    console.log("time:", utcTime)
+    return utcTime
+}
+
+
+
+/**
+ * Convert UTC date to UTC time
+ * @param {Date} date - date to get UTC time of
+ * @returns {number}
+ */
+function date_to_UTC_datetime(date) {
+  let utcDate = new Date(date.toLocaleDateString())
+  return Date.UTC(
+        utcDate.getFullYear(),
+        utcDate.getMonth(),
+        utcDate.getDay()
+    )
+}
+
+///////////////////////OTHER FUNCTIONS FROM FITNESS 2 END
+
+// NOT INCLUDED FROM FITNESS 2 
+
+// make all the files in 'public' available on the Web
+//app.use(express.static('public'))
+
